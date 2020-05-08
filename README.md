69c5805e1fbc737c55b54737cb4eef8f4cdb7d33
dhruvgaloriya <dhruvitgaloriya@gmail.com>
2020-05-08 23:01:51 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# nitrozen-demo
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/nitrozen-demo)
---
+++ src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { ButtonModule } from "nitrozen";
+@NgModule({
+  imports:      [ BrowserModule, FormsModule,ButtonModule],
+  declarations: [ AppComponent, HelloComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
